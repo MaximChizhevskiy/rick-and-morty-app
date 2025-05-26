a4c944e3823166a41f131e38ae675c1934995d75
MaximChizhevskiy <max15.91@mail.ru>
2025-05-26 20:12:00 +0500

    add search

--- src/pages/CharactersPage/CharactersPage.jsx
+++ src/pages/CharactersPage/CharactersPage.jsx
@@ -12,15 +12,22 @@ export const CharactersPage = () => {
         prev: null,
     })
 
+    const [error, setError] = useState(null)
+
     const fetchData = (url) => {
-      axios.get(url).then((res) => {
-          setCharacters(res.data.results)
-          setInfo(res.data.info)
-      })
+        axios.get(url)
+            .then((res) => {
+                setCharacters(res.data.results)
+                setInfo(res.data.info)
+                setError(null)
+            })
+            .catch((error) => {
+                setError(error.response.data.error)
+            })
     }
 
     useEffect(() => {
-      fetchData('https://rickandmortyapi.com/api/character')
+        fetchData('https://rickandmortyapi.com/api/character')
     }, [])
 
     const nextPageHandler = () => {
@@ -31,29 +38,42 @@ export const CharactersPage = () => {
         fetchData(info.prev)
     }
 
+    const searchHandler = (event) => {
+        const value = event.currentTarget.value
+        fetchData(`https://rickandmortyapi.com/api/character?name=${value}`)
+    }
+
     return (
         <div className={'pageContainer'}>
             <h1 className={"pageTitle"}>CharacterPage</h1>
-            <div className={s.characters}>
-                {characters.length && (
-                    characters.map((character) => {
-                        return (
-                            <div key={character.id} className={s.character}>
-                                <div className={s.characterLink}>{character.name}</div>
-                                <img src={character.image} alt={`${character.name} avatar`}/>
-                            </div>
-                        )
-                    })
-                )}
-            </div>
-            <div className={s.buttonContainer}>
-                <button className="linkButton" onClick={previousPageHandler} disabled={info.prev === null}>
-                    Назад
-                </button>
-                <button className="linkButton" onClick={nextPageHandler} disabled={!info.next === null}>
-                    Вперед
-                </button>
-            </div>
+            <input type="search" className={s.search} onChange={searchHandler} placeholder="Search..."/>
+            {error && <div className={'errorMessage'}>{error}</div>}
+
+            {!error && characters.length && (
+                <>
+                    {
+                        <div className={s.characters}>
+                            {characters.map((character) => {
+                                return (
+                                    <div key={character.id} className={s.character}>
+                                        <div className={s.characterLink}>{character.name}</div>
+                                        <img src={character.image} alt={`${character.name} avatar`}/>
+                                    </div>
+                                )
+                            })}
+
+                        </div>
+                    }
+                    <div className={s.buttonContainer}>
+                        <button className="linkButton" onClick={previousPageHandler} disabled={info.prev === null}>
+                            Назад
+                        </button>
+                        <button className="linkButton" onClick={nextPageHandler} disabled={!info.next === null}>
+                            Вперед
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
