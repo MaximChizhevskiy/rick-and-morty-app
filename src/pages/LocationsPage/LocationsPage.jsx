5c27382f5111f3cc4b8e8b8665460cc1f17f0d73
MaximChizhevskiy <max15.91@mail.ru>
2025-05-28 22:05:49 +0500

    add locations page

--- src/pages/LocationsPage/LocationsPage.jsx
+++ src/pages/LocationsPage/LocationsPage.jsx
@@ -1,5 +1,42 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export const LocationsPage = () => {
-    return(
-        <h1 className={'pageTitle'}>LocationsPage</h1>
+    const [locations, setLocations] = useState([])
+
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/location')
+            .then((res) => {
+                setLocations(res.data.results)
+            })
+            .catch((error) => {
+                console.log(error.response.data.error)
+            })
+    }, [])
+
+    return (
+        <div className={'pageContainer'}>
+            <h1 className={'pageTitle'}>LocationsPage</h1>
+            {locations.length && (
+                <>
+                    {locations.map(location => {
+                        return <div>
+                            <ul>
+                                <li>Название локации: <b>{location.name}</b></li>
+                                <li>Тип локации: <b>{location.type}</b></li>
+                                <li>Измерение, в котором находится местоположение: <b>{location.dimension}</b></li>
+                                <li>Количество персонажей, которых видели в данной
+                                    локации: <b>{location.residents.length}</b>
+                                </li>
+
+                            </ul>
+                            <hr/>
+                        </div>
+                    })}
+                </>
+
+            )}
+
+        </div>
     )
 }
